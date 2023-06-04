9115d7017c73d05241e03ef232b5c30712c635d6
xbiotech <biotech9261@gmail.com>
2023-06-04 10:54:32 +0200

    feat: Add function that checks for a win

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const gameBoard = (() => {
 
   function makeMove(player, row, col) {
     board[row][col] = player.marker;
+    gameController.checkWin(board);
   }
 
   createGameBoard();
@@ -32,5 +33,57 @@ const gameBoard = (() => {
 const player = (name, marker) => ({ name, marker });
 
 const john = player("john", "X");
+
+const gameController = (() => {
+  function checkWin(board) {
+    if (
+      (board[0][0] === "X" && board[0][1] === "X" && board[0][2] === "X") ||
+      (board[0][0] === "O" && board[0][1] === "O" && board[0][2] === "O")
+    ) {
+      // return "You Won";
+      console.log("you won horizontal");
+    } else if (
+      (board[1][0] === "X" && board[1][1] === "X" && board[1][2] === "X") ||
+      (board[1][0] === "O" && board[1][1] === "O" && board[1][2] === "O")
+    ) {
+      console.log("you won horizontal");
+    } else if (
+      (board[2][0] === "X" && board[2][1] === "X" && board[2][2] === "X") ||
+      (board[2][0] === "O" && board[2][1] === "O" && board[2][2] === "O")
+    ) {
+      console.log("you won horizontal");
+    } else if (
+      (board[0][0] === "X" && board[1][0] === "X" && board[2][0] === "X") ||
+      (board[0][0] === "O" && board[1][0] === "O" && board[2][0] === "O")
+    ) {
+      console.log("you won vertical");
+    } else if (
+      (board[0][1] === "X" && board[1][1] === "X" && board[2][1] === "X") ||
+      (board[0][1] === "O" && board[1][1] === "O" && board[2][1] === "O")
+    ) {
+      console.log("you won vertical");
+    } else if (
+      (board[0][2] === "X" && board[1][2] === "X" && board[2][2] === "X") ||
+      (board[0][2] === "O" && board[1][2] === "O" && board[2][2] === "O")
+    ) {
+      console.log("you won vertical");
+    } else if (
+      (board[0][0] === "X" && board[1][1] === "X" && board[2][2] === "X") ||
+      (board[0][0] === "O" && board[1][1] === "O" && board[2][2] === "O")
+    ) {
+      console.log("you won diagonal");
+    } else if (
+      (board[0][2] === "X" && board[1][1] === "X" && board[2][0] === "X") ||
+      (board[0][2] === "O" && board[1][1] === "O" && board[2][0] === "O")
+    ) {
+      console.log("you won diagonal");
+    }
+  }
+  return {
+    checkWin,
+  };
+})();
 gameBoard.makeMove(john, [0], [2]);
-gameBoard.makeMove(john, [0], [1]);
+gameBoard.makeMove(john, [1], [1]);
+gameBoard.makeMove(john, [2], [0]);
+console.log(gameBoard.getBoard());
