78ce00145818b3c3e36571118a6399808603df6f
xBioTech <biotech9261@gmail.com>
2023-06-15 20:54:41 +0200

    feat: Add functionality to gameboard buttons when user clicks on a gameboard button the board array updates with the users move

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const gameBoard = (() => {
   const rows = 3;
   const columns = 3;
   const grid = document.querySelector(".grid");
+  const gameboardButtonsArray = [];
+  const player = (name, marker) => ({ name, marker });
+
+  const player1 = player("player1", "X");
+  const player2 = player("player2", "O");
+
+  let currentPlayer = player1;
 
   function createGameBoard() {
     for (let i = 0; i < rows; i++) {
@@ -10,6 +17,10 @@ const gameBoard = (() => {
       for (let j = 0; j < columns; j++) {
         board[i][j] = "";
         const gameboardButtons = document.createElement("button");
+        gameboardButtons.addEventListener("click", () => {
+          makeMove(currentPlayer, i, j);
+        });
+        gameboardButtonsArray.push(gameboardButtons);
         grid.appendChild(gameboardButtons);
       }
     }
@@ -25,12 +36,15 @@ const gameBoard = (() => {
     if (gameController.checkValidMove(board, row, col)) {
       board[row][col] = player.marker;
       gameController.checkWin(board);
-      currentPlayer = gameController.switchPlayerTurn();
+      currentPlayer = gameController.switchPlayerTurn(currentPlayer);
+      console.log(gameBoard.getBoard());
       if (gameController.checkDraw(board) && !gameController.checkWin(board)) {
         console.log("DRAW");
+        console.log(gameBoard.getBoard());
       }
     } else {
       console.log("move invalid");
+      console.log(gameBoard.getBoard());
     }
   }
 
@@ -47,6 +61,7 @@ const gameBoard = (() => {
       }, 200);
     });
   }
+  startNewGame();
 
   function goBackToPlayerSelection() {
     const backBtn = document.querySelector(".back-btn");
@@ -58,125 +73,117 @@ const gameBoard = (() => {
       }, 200);
     });
   }
+  goBackToPlayerSelection();
 
   return {
     board,
     getBoard,
     makeMove,
-    startNewGame,
-    goBackToPlayerSelection,
+    player1,
+    player2,
+    currentPlayer,
   };
 })();
-
-gameBoard.startNewGame();
-gameBoard.goBackToPlayerSelection();
-
-const player = (name, marker) => ({ name, marker });
-
-const player1 = player("john", "X");
-const player2 = player("jane", "O");
-
-let currentPlayer = player1;
 
 const gameController = (() => {
   function checkWin(board) {
     if (
-      board[0][0] === player1.marker &&
-      board[0][1] === player1.marker &&
-      board[0][2] === player1.marker
+      board[0][0] === gameBoard.player1.marker &&
+      board[0][1] === gameBoard.player1.marker &&
+      board[0][2] === gameBoard.player1.marker
     ) {
       // return "You Won";
-      console.log(`${player1.name} won horizontal`);
+      console.log(`${gameBoard.player1.name} won horizontal`);
     } else if (
-      board[0][0] === player2.marker &&
-      board[0][1] === player2.marker &&
-      board[0][2] === player2.marker
+      board[0][0] === gameBoard.player2.marker &&
+      board[0][1] === gameBoard.player2.marker &&
+      board[0][2] === gameBoard.player2.marker
     ) {
-      console.log(`${player2.name} won horizontal`);
+      console.log(`${gameBoard.player2.name} won horizontal`);
     } else if (
-      board[1][0] === player1.marker &&
-      board[1][1] === player1.marker &&
-      board[1][2] === player1.marker
+      board[1][0] === gameBoard.player1.marker &&
+      board[1][1] === gameBoard.player1.marker &&
+      board[1][2] === gameBoard.player1.marker
     ) {
-      console.log(`${player1.name} won horizontal`);
+      console.log(`${gameBoard.player1.name} won horizontal`);
     } else if (
-      board[1][0] === player2.marker &&
-      board[1][1] === player2.marker &&
-      board[1][2] === player2.marker
+      board[1][0] === gameBoard.player2.marker &&
+      board[1][1] === gameBoard.player2.marker &&
+      board[1][2] === gameBoard.player2.marker
     ) {
-      console.log(`${player2.name} won horizontal`);
+      console.log(`${gameBoard.player2.name} won horizontal`);
     } else if (
-      board[2][0] === player1.marker &&
-      board[2][1] === player1.marker &&
-      board[2][2] === player1.marker
+      board[2][0] === gameBoard.player1.marker &&
+      board[2][1] === gameBoard.player1.marker &&
+      board[2][2] === gameBoard.player1.marker
     ) {
-      console.log(`${player1.name} won horizontal`);
+      console.log(`${gameBoard.player1.name} won horizontal`);
     } else if (
-      board[2][0] === player2.marker &&
-      board[2][1] === player2.marker &&
-      board[2][2] === player2.marker
+      board[2][0] === gameBoard.player2.marker &&
+      board[2][1] === gameBoard.player2.marker &&
+      board[2][2] === gameBoard.player2.marker
     ) {
-      console.log(`${player2.name} won horizontal`);
+      console.log(`${gameBoard.player2.name} won horizontal`);
     } else if (
-      board[0][0] === player1.marker &&
-      board[1][0] === player1.marker &&
-      board[2][0] === player1.marker
+      board[0][0] === gameBoard.player1.marker &&
+      board[1][0] === gameBoard.player1.marker &&
+      board[2][0] === gameBoard.player1.marker
     ) {
-      console.log(`${player1.name} won vertical`);
+      console.log(`${gameBoard.player1.name} won vertical`);
     } else if (
-      board[0][0] === player2.marker &&
-      board[1][0] === player2.marker &&
-      board[2][0] === player2.marker
+      board[0][0] === gameBoard.player2.marker &&
+      board[1][0] === gameBoard.player2.marker &&
+      board[2][0] === gameBoard.player2.marker
     ) {
-      console.log(`${player2.name} won vertical`);
+      console.log(`${gameBoard.player2.name} won vertical`);
     } else if (
-      board[0][1] === player1.marker &&
-      board[1][1] === player1.marker &&
-      board[2][1] === player1.marker
+      board[0][1] === gameBoard.player1.marker &&
+      board[1][1] === gameBoard.player1.marker &&
+      board[2][1] === gameBoard.player1.marker
     ) {
-      console.log(`${player1.name} won vertical`);
+      console.log(`${gameBoard.player1.name} won vertical`);
     } else if (
-      board[0][1] === player2.marker &&
-      board[1][1] === player2.marker &&
-      board[2][1] === player2.marker
+      board[0][1] === gameBoard.player2.marker &&
+      board[1][1] === gameBoard.player2.marker &&
+      board[2][1] === gameBoard.player2.marker
     ) {
-      console.log(`${player2.name} won vertical`);
+      console.log(`${gameBoard.player2.name} won vertical`);
     } else if (
-      board[0][2] === player1.marker &&
-      board[1][2] === player1.marker &&
-      board[2][2] === player1.marker
+      board[0][2] === gameBoard.player1.marker &&
+      board[1][2] === gameBoard.player1.marker &&
+      board[2][2] === gameBoard.player1.marker
     ) {
-      console.log(`${player1.name} won vertical`);
+      console.log(`${gameBoard.player1.name} won vertical`);
     } else if (
-      board[0][2] === player2.marker &&
-      board[1][2] === player2.marker &&
-      board[2][2] === player2.marker
+      board[0][2] === gameBoard.player2.marker &&
+      board[1][2] === gameBoard.player2.marker &&
+      board[2][2] === gameBoard.player2.marker
     ) {
-      console.log(`${player2.name} won vertical`);
+      console.log(`${gameBoard.player2.name} won vertical`);
     } else if (
-      board[0][0] === player1.marker &&
-      board[1][1] === player1.marker &&
-      board[2][2] === player1.marker
+      board[0][0] === gameBoard.player1.marker &&
+      board[1][1] === gameBoard.player1.marker &&
+      board[2][2] === gameBoard.player1.marker
     ) {
-      console.log(`${player1.name}won diagonal`);
+      console.log(`${gameBoard.player1.name}won diagonal`);
     } else if (
-      board[0][0] === player2.marker &&
-      board[1][1] === player2.marker &&
-      board[2][2] === player2.marker
+      board[0][0] === gameBoard.player2.marker &&
+      board[1][1] === gameBoard.player2.marker &&
+      board[2][2] === gameBoard.player2.marker
     ) {
-      console.log(`${player2.name}won diagonal`);
+      console.log(`${gameBoard.player2.name}won diagonal`);
     } else if (
-      board[0][2] === player1.marker &&
-      board[1][1] === player1.marker &&
-      board[2][0] === player1.marker
+      board[0][2] === gameBoard.player1.marker &&
+      board[1][1] === gameBoard.player1.marker &&
+      board[2][0] === gameBoard.player1.marker
     ) {
-      console.log(`${player1.name}won diagonal`);
+      console.log(`${gameBoard.player1.name}won diagonal`);
     } else if (
-      board[0][2] === player2.marker &&
-      board[1][1] === player2.marker &&
-      board[2][0] === player2.marker
+      board[0][2] === gameBoard.player2.marker &&
+      board[1][1] === gameBoard.player2.marker &&
+      board[2][0] === gameBoard.player2.marker
     ) {
-      console.log(`${player2.name}won diagonal`);
+      console.log(`${gameBoard.player2.name}won diagonal`);
     }
   }
 
@@ -198,11 +205,11 @@ const gameController = (() => {
     return false;
   }
 
-  function switchPlayerTurn() {
-    if (currentPlayer === player1) {
-      return player2;
+  function switchPlayerTurn(currentPlayer) {
+    if (currentPlayer === gameBoard.player1) {
+      return gameBoard.player2;
     }
-    return player1;
+    return gameBoard.player1;
   }
   return {
     checkWin,
@@ -211,7 +218,3 @@ const gameController = (() => {
     checkValidMove,
   };
 })();
-gameBoard.makeMove(currentPlayer, 1, 1);
-gameBoard.makeMove(currentPlayer, 1, 1);
-gameBoard.makeMove(currentPlayer, 2, 1);
-console.log(gameBoard.getBoard());
